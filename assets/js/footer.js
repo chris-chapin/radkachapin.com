fd2218539c35201f651e0118174f7a5e2826e421
Chris Chapin <cchapin@outlook.com>
2024-01-16 11:06:55 -0800

    fix: footer font size too small on phone

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,21 +1,23 @@
 class Footer extends HTMLElement {
   connectedCallback() {
     const year = new Date().getFullYear();
-    const copyright = `Copyright \u00A9 2015-${year} Radka Chapin Counseling PLLC | Bellevue, WA`;
+    const copyright = `Copyright \u00A9 2015-${year} Radka Chapin Counseling PLLC | Redmond, WA`;
 
     this.innerHTML = `
     <center>
-      <p>${copyright}</p>
-      <p>
+      <p style="font-size:16px;">
         <a href="/.">Home</a> | 
         <a href="/about">About</a> | <a href="/specialties">Specialties</a> | 
         <a href="/forms">Forms</a> | <a href="/insurance">Insurance</a> | 
         <a href="/contact">Contact</a> | 
         <a href="/resources">Resources</a>
     </p>
+    <br />
     <p>
     <a href="mailto:radka@radkachapin.com">radka@radkachapin.com</a>
     </p>
+    <br />
+    <p>${copyright}</p>
     <br />
     </center>`;
   }
